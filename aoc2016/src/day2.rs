1c50f13b800f24fbf0c0d3382991291accf6717a
Avraham Appel <aappel248@gmail.com>
2024-11-06 16:41:50 -0500

    Start 2016 day 2 part 1

---
+++ aoc2016/src/day2.rs
@@ -0,0 +1,81 @@
+use aoc_runner_derive::{aoc, aoc_generator};
+
+enum Dir {
+    U,
+    D,
+    R,
+    L,
+}
+
+type Digit = (usize, usize);
+type Dirs = Vec<Dir>;
+type Lines = Vec<Dirs>;
+
+#[aoc_generator(day2)]
+fn parse(input: &str) -> Lines {
+    input
+        .lines()
+        .map(|l| {
+            l.trim()
+                .chars()
+                .filter_map(|c| match c {
+                    'U' => Some(Dir::U),
+                    'D' => Some(Dir::D),
+                    'R' => Some(Dir::R),
+                    'L' => Some(Dir::L),
+                    _ => None,
+                })
+                .collect()
+        })
+        .collect()
+}
+
+#[aoc(day2, part1)]
+fn part1(input: &Lines) -> String {
+    let digits = input.iter().fold(vec![], |mut digits: Vec<Digit>, line| {
+        let new_digit = line.iter().fold(
+            digits.last().copied().unwrap_or((1, 1)),
+            |(x, y), dir| match dir {
+                Dir::U => (x, if y > 0 { y - 1 } else { 0 }),
+                Dir::D => (x, if y < 3 { y + 1 } else { 3 }),
+                Dir::L => (if x > 0 { x - 1 } else { 0 }, y),
+                Dir::R => (if x < 3 { x + 1 } else { 3 }, y),
+            },
+        );
+        digits.push(new_digit);
+        digits
+    });
+
+    digits
+        .into_iter()
+        .map(|(x, y)| ((x + 1) * (y + 1)).to_string())
+        .collect()
+}
+
+#[aoc(day2, part2)]
+fn part2(input: &Lines) -> String {
+    todo!()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn part1_example() {
+        assert_eq!(
+            part1(&parse(
+                "ULL
+                 RRDDD
+                 LURDL
+                 UUUUD"
+            )),
+            "1985"
+        );
+    }
+
+    #[test]
+    fn part2_example() {
+        assert_eq!(part2(&parse("<EXAMPLE>")), "<RESULT>");
+    }
+}
