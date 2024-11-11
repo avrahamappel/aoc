040515eb6527ad1613d57b95e4b8387a23ba675d
Avraham Appel <aappel248@gmail.com>
2024-11-11 00:17:47 -0500

    Try part 2

--- aoc2023/src/day1.rs
+++ aoc2023/src/day1.rs
@@ -22,9 +22,59 @@ fn part1(input: &[String]) -> u32 {
         .sum()
 }
 
+/// Parse all digits or numeric digit strings from a string
+/// Returns a Vec of chars that are valid ascii digits
+fn parse_digits(s: &str) -> Vec<char> {
+    let digit_words = [
+        "one", "two", "three", "four", "five", "six", "seven", "eight", "nine",
+    ];
+    // Found digits in here
+    let mut digits = vec![];
+    // Track current alphabetical word
+    let mut cur_word = String::new();
+
+    for c in s.chars() {
+        if c.is_ascii_digit() {
+            digits.push(c);
+            cur_word.clear();
+        } else {
+            cur_word.push(c)
+        }
+
+        // Check cur_word for digit match
+        if let Some(pos) = digit_words.iter().position(|dw| cur_word.ends_with(dw)) {
+            // Increment index by 1 to find the char value
+            digits.push(char::from_digit(pos as u32 + 1, 10).unwrap());
+            cur_word.clear();
+        }
+    }
+
+    digits
+}
+
+// TODO Doesn't work. Perhaps we need to use parse_digits backwards for the second number, just as
+// we do for the first?
 #[aoc(day1, part2)]
 fn part2(input: &[String]) -> u32 {
-    todo!()
+    input
+        .iter()
+        .filter_map(|line| {
+            eprintln!();
+            eprintln!("---LINE---");
+            dbg!(line);
+            let digits = parse_digits(line);
+            dbg!(&digits);
+            if let (Some(f), Some(l)) = (digits.first(), digits.last()) {
+                Some(format!("{f}{l}"))
+            } else {
+                None
+            }
+        })
+        .map(|d| d.parse::<u32>().unwrap())
+        .inspect(|x| {
+            dbg!(x);
+        })
+        .sum()
 }
 
 #[cfg(test)]
