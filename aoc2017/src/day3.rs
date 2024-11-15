e9c2f45712d8bb19ec5c4e7c1b39a9229609025f
Avraham Appel <aappel248@gmail.com>
2024-11-15 11:41:54 -0500

    Start part 2 and discover bug in Layer::pos_of

--- aoc2017/src/day3.rs
+++ aoc2017/src/day3.rs
@@ -1,3 +1,5 @@
+use std::collections::HashMap;
+
 use aoc_runner_derive::{aoc, aoc_generator};
 
 #[aoc_generator(day3)]
@@ -26,10 +28,10 @@ impl Layer {
     }
 
     fn pos_of(self, val: i32) -> (i32, i32) {
-        //dbg!(self.id, self.start, self.end);
+        dbg!(self.id, self.start, self.end);
         //assert!(self.contains(val));
         let offset = val - self.start;
-        //dbg!(offset);
+        dbg!(offset);
 
         // calculate corners
         let k = self.id;
@@ -53,7 +55,7 @@ impl Layer {
 /// Algorithm by chatgpt
 fn grid_pos(int: i32) -> (i32, i32) {
     // Determine the "layer" around the starting point
-    if let Some(layer) = (1..).map(Layer::new).find(|l| l.contains(int)) {
+    if let Some(layer) = (1..i32::MAX).map(Layer::new).find(|l| l.contains(int)) {
         layer.pos_of(int)
     } else {
         unimplemented!()
@@ -73,8 +75,43 @@ fn part1(input: &i32) -> i32 {
 }
 
 #[aoc(day3, part2)]
-fn part2(input: &i32) -> String {
-    todo!()
+fn part2(input: &i32) -> i32 {
+    let mut grid = HashMap::from([((0, 0), 1)]);
+    for int in 2..i32::MAX {
+        //eprintln!();
+        //eprintln!("---DEBUG---");
+        //dbg!(int);
+        // Generate grid, populating each cell with the sum of populated neighbors
+        let pos = grid_pos(int);
+        //dbg!(pos);
+        let get_neighbor_coords = |(x, y)| {
+            [
+                (x - 1, y - 1),
+                (x, y - 1),
+                (x + 1, y - 1),
+                (x - 1, y),
+                (x + 1, y),
+                (x - 1, y + 1),
+                (x, y + 1),
+                (x + 1, y + 1),
+            ]
+        };
+        let neighbors = get_neighbor_coords(pos);
+        let value = grid
+            .iter()
+            .filter(|(p, _)| (neighbors.contains(p)))
+            .map(|(_, v)| v)
+            .sum();
+        //dbg!(value);
+
+        // When the value is higher than `input`, return the value
+        if value > *input {
+            return value;
+        }
+
+        grid.insert(pos, value);
+    }
+    0
 }
 
 #[cfg(test)]
@@ -83,13 +120,30 @@ mod tests {
 
     #[test]
     fn part1_example() {
-        for (input, output) in [(1, 0), (12, 3), (23, 2), (28, 3), (41, 4), (1024, 31)] {
+        for (input, output) in [
+            (1, 0),
+            (2, 1),
+            (3, 2),
+            (4, 1),
+            (5, 2),
+            (6, 1),
+            (7, 2),
+            (8, 1),
+            (9, 2),
+            (12, 3),
+            (23, 2),
+            (28, 3),
+            (41, 4),
+            (1024, 31),
+        ] {
             assert_eq!(part1(&input), output);
         }
     }
 
     #[test]
     fn part2_example() {
-        assert_eq!(part2(&parse("<EXAMPLE>")), "<RESULT>");
+        for (input, output) in [(3, 4), (6, 10), (12, 23), (24, 25)] {
+            assert_eq!(part2(&input), output);
+        }
     }
 }
