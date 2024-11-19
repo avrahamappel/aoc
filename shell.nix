ab98958199c71db8a0e1c7e78d19e2f5ed8654e4
Avraham Appel <aappel248@gmail.com>
2024-11-18 23:37:31 -0500

    Pin unstable nixpkgs

--- shell.nix
+++ shell.nix
@@ -1,20 +1,24 @@
-{ pkgs ? import <nixpkgs> { } }:
-
 let
-  cargo-aoc-src = (import ./npins).cargo-aoc;
+  pins = import ./npins;
+
+  pkgs = import pins.nixpkgs-unstable { };
+
+  inherit (pkgs) lib stdenv;
+
+  cargo-aoc-src = pins.cargo-aoc;
   cargo-aoc = pkgs.rustPlatform.buildRustPackage {
     pname = "cargo-aoc";
     version = cargo-aoc-src.version;
     src = cargo-aoc-src;
     cargoLock.lockFile = "${cargo-aoc-src}/Cargo.lock";
 
-    buildInputs = pkgs.lib.optionals pkgs.stdenv.isDarwin
+    buildInputs = lib.optionals stdenv.isDarwin
       (with pkgs.darwin.apple_sdk.frameworks; [
         CoreServices
         SystemConfiguration
       ]);
 
-    nativeBuildInputs = pkgs.lib.optional pkgs.stdenv.isDarwin pkgs.pkg-config;
+    nativeBuildInputs = lib.optional stdenv.isDarwin pkgs.pkg-config;
   };
 in
 
@@ -29,5 +33,5 @@ pkgs.mkShell {
     rustfmt
   ] ++ lib.optional stdenv.isDarwin iconv;
 
-  nativeBuildInputs = pkgs.lib.optional pkgs.stdenv.isDarwin pkgs.pkg-config;
+  nativeBuildInputs = lib.optional stdenv.isDarwin pkgs.pkg-config;
 }
