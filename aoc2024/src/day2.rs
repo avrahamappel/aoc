4ef288b33b7a0ef54ead87cc14cd1becab76a28c
Avraham Appel <aappel248@gmail.com>
2025-01-06 11:08:04 -0500

    2024 day 2 part 2

--- aoc2024/src/day2.rs
+++ aoc2024/src/day2.rs
@@ -16,45 +16,49 @@ fn parse(input: &str) -> Input {
         .collect()
 }
 
+fn check_report(report: &Report, i: usize) -> bool {
+    let mut incr = None;
+    let mut prev = None;
+    for lvl in report {
+        //eprintln!("{prev:?} {lvl} {incr:?}");
+        if let Some(prv) = prev {
+            if incr.is_none() {
+                incr = Some(lvl > prv);
+            }
+            match incr {
+                Some(true) => {
+                    if lvl <= prv || lvl - prv > 3 {
+                        eprintln!(
+                            "{}: Increase error: {lvl} does not increase correctly from {prv}",
+                            i + 1
+                        );
+                        return false;
+                    }
+                }
+                Some(false) => {
+                    if lvl >= prv || prv - lvl > 3 {
+                        eprintln!(
+                            "{}: Decrease error: {lvl} does not decrease correctly from {prv}",
+                            i + 1
+                        );
+                        return false;
+                    }
+                }
+                _ => unreachable!(),
+            }
+        }
+        prev = Some(lvl);
+    }
+    eprintln!("{}: Safe", i + 1);
+    true
+}
+
 #[aoc(day2, part1)]
 fn part1(input: &Input) -> usize {
     input
         .iter()
         .enumerate()
-        .filter(|(i, report)| {
-            let mut incr = None;
-            let mut prev = None;
-            for lvl in *report {
-                //eprintln!("{prev:?} {lvl} {incr:?}");
-                if let Some(prv) = prev {
-                    if incr.is_none() {
-                            incr = Some(lvl > prv);
-                    }
-                    match incr {
-                        Some(true) => {
-                            if lvl <= prv || lvl - prv > 3 {
-                                eprintln!(
-                                    "{}: Increase error: {lvl} does not increase correctly from {prv}",
-                                i + 1);
-                                return false;
-                            }
-                        },
-                        Some(false) => {
-                            if lvl >= prv || prv - lvl > 3 {
-                                eprintln!(
-                                    "{}: Decrease error: {lvl} does not decrease correctly from {prv}",
-                                i + 1);
-                                return false;
-                            }
-                        },
-                        _ => unreachable!()
-                    }
-                }
-                prev = Some(lvl);
-            }
-            eprintln!("{}: Safe", i + 1);
-            true
-        })
+        .filter(|(i, report)| check_report(report, *i))
         //.inspect(|_| {
         //    eprintln!("ok");
         //})
@@ -62,8 +66,29 @@ fn part1(input: &Input) -> usize {
 }
 
 #[aoc(day2, part2)]
-fn part2(input: &Input) -> String {
-    todo!()
+fn part2(input: &Input) -> usize {
+    input
+        .iter()
+        .enumerate()
+        .filter(|(i, report)| {
+            if check_report(*report, *i) {
+                return true;
+            }
+
+            // try removing each level
+            // could make this smarter and only remove ones that are near the area of error, but
+            // this is probably fine for now
+            for j in 0..report.len() {
+                let mut r = (*report).clone();
+                r.remove(j);
+                if check_report(&r, *i) {
+                    return true;
+                }
+            }
+
+            false
+        })
+        .count()
 }
 
 #[cfg(test)]
@@ -87,6 +112,16 @@ mod tests {
 
     #[test]
     fn part2_example() {
-        assert_eq!(part2(&parse("<EXAMPLE>")), "<RESULT>");
+        assert_eq!(
+            part2(&parse(
+                "7 6 4 2 1
+                 1 2 7 8 9
+                 9 7 6 2 1
+                 1 3 2 4 5
+                 8 6 4 4 1
+                 1 3 6 7 9"
+            )),
+            4
+        );
     }
 }
