305a70c325b7d506ae6bb836b17d1b151218a71a
Avraham Appel <aappel248@gmail.com>
2025-01-07 00:41:34 -0500

    2016 day 5 part 2

--- aoc2016/src/day5.rs
+++ aoc2016/src/day5.rs
@@ -28,7 +28,32 @@ fn part1(input: &str) -> String {
 
 #[aoc(day5, part2)]
 fn part2(input: &str) -> String {
-    todo!()
+    let mut password = String::from("________");
+    let mut i = 0;
+
+    while password.contains('_') {
+        let val = format!("{input}{i}");
+        eprint!("string:    {val}     ");
+        let digest = md5::compute(val);
+        let hash = format!("{digest:x}");
+        //eprintln!("hash:    {hash}");
+        if hash.starts_with("00000") {
+            let pos = hash.chars().nth(5).unwrap();
+            if pos.is_ascii_digit() {
+                let pos = pos.to_digit(10).unwrap();
+                if pos < 8 && password.chars().nth(pos as usize).unwrap() == '_' {
+                    password.replace_range(
+                        pos as usize..=pos as usize,
+                        &hash.chars().nth(6).unwrap().to_string(),
+                    );
+                }
+            }
+        }
+        eprintln!("password:  {password}");
+        i += 1;
+    }
+
+    password
 }
 
 #[cfg(test)]
@@ -42,6 +67,6 @@ mod tests {
 
     #[test]
     fn part2_example() {
-        assert_eq!(part2(&parse("<EXAMPLE>")), "<RESULT>");
+        assert_eq!(part2(&parse("abc")), "05ace8e3");
     }
 }
