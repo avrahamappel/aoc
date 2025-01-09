aa0496f42507ce63806649edcae3be6539f4c6f1
Avraham Appel <aappel248@gmail.com>
2025-01-08 23:01:42 -0500

    2020 day 7 part 2

--- aoc2020/src/day7.rs
+++ aoc2020/src/day7.rs
@@ -62,8 +62,27 @@ fn part1(input: &Input) -> usize {
 }
 
 #[aoc(day7, part2)]
-fn part2(input: &Input) -> String {
-    todo!()
+fn part2(input: &Input) -> usize {
+    fn count_bags(bag: &BagColor, rules: &Input, i: usize) -> usize {
+        let indent = "  ".repeat(i);
+        eprintln!("{indent}Counting contents of bag [{bag}]");
+        let rule = rules.get(bag).unwrap();
+        if rule.is_empty() {
+            eprintln!("{indent}No bags");
+            1
+        } else {
+            let total: usize = rule
+                .iter()
+                .map(|(color, count)| {
+                    eprintln!("{indent}color: {color}, count: {count}");
+                    count * count_bags(color, rules, i + 1)
+                })
+                .sum();
+            eprintln!("{indent}bag [{bag}] total: {total}");
+            total + 1
+        }
+    }
+    count_bags(&String::from("shiny gold"), input, 0) - 1
 }
 
 #[cfg(test)]
@@ -90,6 +109,31 @@ mod tests {
 
     #[test]
     fn part2_example() {
-        assert_eq!(part2(&parse("<EXAMPLE>")), "<RESULT>");
+        for (input, output) in [
+            (
+                "light red bags contain 1 bright white bag, 2 muted yellow bags.
+                 dark orange bags contain 3 bright white bags, 4 muted yellow bags.
+                 bright white bags contain 1 shiny gold bag.
+                 muted yellow bags contain 2 shiny gold bags, 9 faded blue bags.
+                 shiny gold bags contain 1 dark olive bag, 2 vibrant plum bags.
+                 dark olive bags contain 3 faded blue bags, 4 dotted black bags.
+                 vibrant plum bags contain 5 faded blue bags, 6 dotted black bags.
+                 faded blue bags contain no other bags.
+                 dotted black bags contain no other bags.",
+                32,
+            ),
+            (
+                "shiny gold bags contain 2 dark red bags.
+                 dark red bags contain 2 dark orange bags.
+                 dark orange bags contain 2 dark yellow bags.
+                 dark yellow bags contain 2 dark green bags.
+                 dark green bags contain 2 dark blue bags.
+                 dark blue bags contain 2 dark violet bags.
+                 dark violet bags contain no other bags.",
+                126,
+            ),
+        ] {
+            assert_eq!(part2(&parse(input)), output);
+        }
     }
 }
