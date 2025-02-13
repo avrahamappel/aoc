a8ea8611461749a34cc701d3485e5a655501a4fa
Avraham Appel <aappel248@gmail.com>
2025-02-12 23:42:04 -0500

    2019 day 6 part 2

--- aoc2019/src/day6.rs
+++ aoc2019/src/day6.rs
@@ -1,4 +1,4 @@
-use std::collections::HashMap;
+use std::collections::{HashMap, HashSet, VecDeque};
 
 use aoc_runner_derive::{aoc, aoc_generator};
 
@@ -20,7 +20,7 @@ fn parse(input: &str) -> Input {
 #[aoc(day6, part1)]
 fn part1(input: &Input) -> u32 {
     let mut orbits: HashMap<&str, u32> = input.iter().map(|kv| (kv.1.as_str(), 0)).collect();
-    let mut searches = std::collections::VecDeque::from(["COM"]);
+    let mut searches = VecDeque::from(["COM"]);
     while let Some(s) = searches.pop_front() {
         // find all objects that orbit this target
         let objs: Vec<_> = input
@@ -40,8 +40,32 @@ fn part1(input: &Input) -> u32 {
 }
 
 #[aoc(day6, part2)]
-fn part2(input: &Input) -> String {
-    todo!()
+fn part2(input: &Input) -> u32 {
+    let mut searches = VecDeque::from([(0, "YOU")]);
+    let mut visited = HashSet::new();
+    while let Some((mut dist, s)) = searches.pop_front() {
+        //dbg!(dist, s);
+        if visited.contains(&s) {
+            continue;
+        }
+        visited.insert(s);
+        if s == "SAN" {
+            return dist - 2;
+        }
+        dist += 1;
+        // Add the target that the search obj is orbiting around (might be null)
+        if let Some((tar, _)) = input.iter().find(|(_, o)| o == s) {
+            searches.push_back((dist, tar.as_str()));
+        }
+        // Add all objects orbiting around the search obj
+        searches.extend(
+            input
+                .iter()
+                .filter(|(tar, _)| tar == s)
+                .map(|(_, obj)| (dist, obj.as_str())),
+        );
+    }
+    unreachable!()
 }
 
 #[cfg(test)]
@@ -70,6 +94,23 @@ mod tests {
 
     #[test]
     fn part2_example() {
-        assert_eq!(part2(&parse("<EXAMPLE>")), "<RESULT>");
+        assert_eq!(
+            part2(&parse(
+                "COM)B
+                 B)C
+                 C)D
+                 D)E
+                 E)F
+                 B)G
+                 G)H
+                 D)I
+                 E)J
+                 J)K
+                 K)L
+                 K)YOU
+                 I)SAN"
+            )),
+            4
+        );
     }
 }
