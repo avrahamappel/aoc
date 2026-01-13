1bc80bbb8c2a028fad9c1fc7ec92d7e020c65d20
Avraham Appel <aappel248@gmail.com>
2026-01-13 11:16:54 -0500

    Update deps and remove npins
    cargo-aoc has been added to nixpkgs

--- flake.nix
+++ flake.nix
@@ -9,24 +9,6 @@
       pkgs = import nixpkgs { inherit system; };
 
       inherit (pkgs) lib stdenv;
-
-      pins = import ./npins;
-
-      cargo-aoc-src = pins.cargo-aoc;
-      cargo-aoc = pkgs.rustPlatform.buildRustPackage {
-        pname = "cargo-aoc";
-        version = cargo-aoc-src.version;
-        src = cargo-aoc-src;
-        cargoLock.lockFile = "${cargo-aoc-src}/Cargo.lock";
-
-        buildInputs = lib.optionals stdenv.isDarwin
-          (with pkgs.darwin.apple_sdk.frameworks; [
-            CoreServices
-            SystemConfiguration
-          ]);
-
-        nativeBuildInputs = lib.optional stdenv.isDarwin pkgs.pkg-config;
-      };
     in
     {
       devShells.default = pkgs.mkShell {
